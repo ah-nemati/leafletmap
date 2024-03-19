829040b199ebeb8be9cf64deb23f25ae90d16398
ah-nemati <nemati.Amh@gmail.com>
2024-03-19 12:26:57 +0330

    leaflet shapes edit remove create handeled

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -5,7 +5,7 @@ import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
 import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman";
 
 const LeafletMap = () => {
-  const [geoJson, setGeoJson] = useState<any[]>([]);
+  const [geoJson, setGeoJson] = useState([]);
 
   useEffect(() => {
     const map = L.map("leafletmap").setView(
@@ -17,16 +17,32 @@ const LeafletMap = () => {
     map.pm.addControls({
       drawControls: true,
       editControls: true,
-      optionsControls: true,
-      customControls: true,
+      optionsControls: false,
+      customControls: false,
       oneBlock: false,
     });
 
-    
+    map.on("pm:create", (e) => {
+      //@ts-ignore
+      setGeoJson([...map.pm.getGeomanLayers(true).toGeoJSON().features]);
+      e.layer.on("pm:edit", () => {
+        //@ts-ignore
+        setGeoJson([...map.pm.getGeomanLayers(true).toGeoJSON().features]);
+      });
+    });
+
+    map.on("pm:remove", () => {
+      //@ts-ignore
+      setGeoJson([...map.pm.getGeomanLayers(true).toGeoJSON().features]);
+    });
+
+    const geoJsonLayer = L.geoJSON(geoJson).addTo(map);
+
     return () => {
       map.remove();
+      map.removeLayer(geoJsonLayer);
     };
-  }, [geoJson]);
+  }, []);
 
   return <div className="w-1/2 h-full" id="leafletmap"></div>;
 };
